f3eb956565544d67a996592d46eae95ca202bcd1
Biki Tirua <bikitirua321@gmail.com>
2024-03-15 07:47:27 +0530

    build:v0.06

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,24 +3,24 @@ import Image from "next/image";
 const Homepage = () => {
   return (
     <>
-      <div className="h-full flex flex-col px-4 sm:px-8 md-px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md-px-12 lg:px-20 xl:px-48">
 
         {/* Image container*/}
-        <div className="h-1/2 relative">
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
           <Image src="/hero.png" alt="logo" fill className="object-contain" />
         </div>
 
         {/*text container */}
-        <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
 
           {/* Title */}
-          <h1 className="text-4xl font-bold">Lorem ipsum dolor sit amet consectetur ?</h1>
+          <h1 className="text-4xl md:text-6xl font-bold">Lorem ipsum dolor sit amet consectetur ?</h1>
 
           {/* Description */}
-          <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ab distinctio natus maxime quibusdam nihil mollitia recusandae, numquam reprehenderit magni voluptas cumque qui in animi nobis rerum molestiae nam tempora.</p>
+          <p className="md:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ab distinctio natus maxime quibusdam nihil mollitia recusandae, numquam reprehenderit magni voluptas cumque qui in animi nobis rerum molestiae nam tempora.</p>
 
           {/* btn group */}
-          <div className="flex gap-4">
+          <div className="flex w-full gap-4">
             <button className="p-4 rounded ring-1 ring-black">View My Work</button>
             <button className="p-4 rounded ring-1 ring-black bg-black text-white">Contact Me</button>
           </div>
---
+++ src/app/testanime/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+import React from 'react'
+import { motion } from 'framer-motion'
+const TestPage = () => {
+
+    const variants = {
+        variant1: {
+            x: 400,
+            y: 300,
+            opacity: 0.5,
+        },
+        variant2: {
+            x: 100,
+            y: -300,
+            rotation: 90,
+        },
+    }
+
+
+    return (
+        <div className=' flex justify-center items-center h-full'>
+            <motion.h1
+                className='p-3 bg-blue-300'
+
+                // initial={{
+                //     opacity: 0,
+                //     x: -100
+                // }}
+                variants={variants}
+                animate="variant2"
+                // animate={{
+                //     opacity: 1,
+                //     x: 60
+                // }}
+                // transition={{
+                //     duration: 1
+                // }}
+
+            >Hello Framer Motion</motion.h1>
+        </div>
+    )
+}
+
+export default TestPage
